0d31d4a81eae86c68ff2cc47cfa075440bada59c
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-22 23:54:57 -0500

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,24 +1,9 @@
-#!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.amenity import Amenity
-from models.review import Review
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
-from sqlalchemy.orm import relationship
-import models
-
-place_amenity = Table('place_amenity', Base.metadata, Column('place_id',
-                      String(60), ForeignKey('places.id'), nullable=False),
-                      Column('amenity_id', String(60),
-                      ForeignKey('amenities.id'), nullable=False))
-
-
 class Place(BaseModel, Base):
     """ A place to stay """
     if models.type_storage == "db":
         __tablename__ = "places"
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        user_id = Column(String(60),  ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
@@ -27,7 +12,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="place", cascade="delete")
+
         amenities = relationship("Amenity", secondary=place_amenity,
                                  viewonly=False)
     else:
@@ -45,7 +30,6 @@ class Place(BaseModel, Base):
 
         @property
         def reviews(self):
-        """Get a list of all linked Reviews."""
             return models.storage.all(Review)
 
         @property
