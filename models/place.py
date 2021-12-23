22b03306751378c092cfd471e34635c6b8ede698
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-22 23:53:16 -0500

    Update place.py

--- models/place.py
+++ models/place.py
@@ -18,7 +18,7 @@ class Place(BaseModel, Base):
     if models.type_storage == "db":
         __tablename__ = "places"
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60),  ForeignKey('users.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
@@ -27,7 +27,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-
+        reviews = relationship("Review", backref="place", cascade="delete")
         amenities = relationship("Amenity", secondary=place_amenity,
                                  viewonly=False)
     else:
@@ -45,6 +45,7 @@ class Place(BaseModel, Base):
 
         @property
         def reviews(self):
+        """Get a list of all linked Reviews."""
             return models.storage.all(Review)
 
         @property
