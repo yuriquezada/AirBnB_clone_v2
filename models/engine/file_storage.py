5dcbf515a6c9453189af227cc23aa896e4474bea
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-12-22 23:20:19 -0500

    Add try-except in delete function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,7 +61,8 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """ delete obj from __objects """
-        if obj:
-            del self.__objects[obj.__class__.__name__ + '.' + obj.id]
-            self.save()
+        """Delete a given object from __objects, if it exists."""
+        try:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
+        except (AttributeError, KeyError):
+            pass
