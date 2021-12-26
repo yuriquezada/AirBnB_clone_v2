5684c1170446627fcc5b8cd0066b305f3f918bb1
sammysamp <sammysamp@gmail.com>
2021-12-25 18:19:24 -0800

    fix file storage type

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
+
 Base = declarative_base()
 
 
@@ -21,12 +22,25 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__" and k != "_sa_instance_state":
+                    setattr(self, k, v)
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+            if not self.id:
+                self.id = str(uuid.uuid4())
+
+            #     setattr(self, k, v)
+            # kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            #                                          '%Y-%m-%dT%H:%M:%S.%f')
+            # kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            #                                          '%Y-%m-%dT%H:%M:%S.%f')
+            # del kwargs['__class__']
+            # self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -53,6 +67,6 @@ class BaseModel:
         return dictionary
 
     def delete(self):
-        '''Delete the current instance from the storage'''
+        '''To delete the current instance from the storage'''
         from models import storage
         storage.delete(self)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,22 +13,27 @@ class FileStorage:
         Return:
             returns a dictionary of __object
         """
-        if cls is not None:
-            if type(cls) == str:
-                cls = eval(cls)
-            cls_dict = {}
-            for k, v in self.__objects.items():
-                if type(v) == cls:
-                    cls_dict[k] = v
-            return cls_dict
-        return self.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            # print("CLASE: {}".format(cls))
+            my_dict = {}
+            # print("OBJECTS: {}".format(self.__objects))
+            for key in self.__objects:
+                name = key.split('.')
+                # print("NAME: {}".format(name))
+                # print("CLSNAME: {}".format(cls.__name__))
+                if name[0] == cls.__name__:
+                    my_dict[key] = self.__objects[key]
+            # print("MY_DICT: {}".format(my_dict))
+            return my_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """ Saves storage dictionary to a file """
+        """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
@@ -40,9 +45,9 @@ class FileStorage:
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
         from models.user import User
-        from models.place import Place
         from models.state import State
         from models.city import City
+        from models.place import Place
         from models.amenity import Amenity
         from models.review import Review
 
@@ -56,17 +61,12 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """Delete a given object from __objects, if it exists."""
-        try:
-            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
-        except (AttributeError, KeyError):
-            pass
-        
-    def close(self):
-        """Call the reload method."""
-        self.reload()
+        """ delete obj from __objects """
+        if obj:
+            del self.__objects[obj.__class__.__name__ + '.' + obj.id]
+            self.save()
