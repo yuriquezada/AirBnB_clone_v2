eca2180450ab6b109e892da68ceefffc4fd47d40
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2022-01-30 22:30:54 -0500

    Create 5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,50 @@
+'''
+First program using Flask framework
+'''
+from email.policy import default
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    '''Print a message from Flask'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''Print a message from Flask'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is(text):
+    '''Print a message from Flask'''
+    return 'C ' + text.replace("_", " ")
+
+
+@app.route('/python', defaults={'text': 'is_cool'})
+@app.route('/python/<text>')
+def python_is_cool(text):
+    '''Print a message from Flask'''
+    return 'Python ' + text.replace("_", " ")
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    '''Print a message from Flask'''
+    return '{:d} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    '''Print a message from Flask'''
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
