565af69263feb350fb92f252944c2a712c6f5f34
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2022-01-30 22:40:25 -0500

    Update 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,47 +1,57 @@
 #!/usr/bin/python3
-# Script that starts a Flask web application
-
+'''
+First program using Flask framework
+'''
+from email.policy import default
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def index():
-    return "Hello HBNB!"
+def hello():
+    '''Print a message from Flask'''
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    return "HBNB"
+    '''Print a message from Flask'''
+    return 'HBNB'
 
 
-@app.route('/c/<string:s>')
-def c(s):
-    new_s = s.replace("_", " ")
-    return "C {}".format(new_s)
+@app.route('/c/<text>')
+def c_is(text):
+    '''Print a message from Flask'''
+    return 'C ' + text.replace("_", " ")
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<string:s>')
-def python(s="is cool"):
-    new_s = s.replace("_", " ")
-    return "Python {}".format(new_s)
+@app.route('/python', defaults={'text': 'is_cool'})
+@app.route('/python/<text>')
+def python_is_cool(text):
+    '''Print a message from Flask'''
+    return 'Python ' + text.replace("_", " ")
 
 
 @app.route('/number/<int:n>')
 def number(n):
-    return "{} is a number".format(n)
+    '''Print a message from Flask'''
+    return '{:d} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
-    return render_template('5-number.html', num=n)
+    '''Print a message from Flask'''
+    return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', num=n)
+    '''Print a message from Flask'''
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
