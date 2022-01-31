403ff4bb9f4071aa974e4ed94c6bf291e4d4f72e
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2022-01-30 22:02:26 -0500

    Create 0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+First program using Flask framework
+'''
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    '''Print a message from Flask'''
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
